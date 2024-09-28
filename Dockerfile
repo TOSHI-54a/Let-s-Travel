847adb37a772f804226a9e66242fceeb7e1d3c07
apple <apple@Tottotto-MacBook-Air.local>
2024-09-28 17:07:01 +0900

    Add_heroku

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ruby:3.1.4
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs npm
+RUN npm install -g yarn esbuild sass vite
+RUN mkdir /lets_travel
+WORKDIR /lets_travel
+RUN gem install bundler:2.5.6
+RUN gem install foreman
+COPY Gemfile /lets_travel/Gemfile
+COPY Gemfile.lock /lets_travel/Gemfile.lock
+COPY . /lets_travel
+RUN bundle install
+RUN yarn install
+RUN rails assets:precompile
+CMD ["rails", "server", "-b", "0.0.0.0"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[new create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :user_sessions, only: %i[new create destroy]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create show edit destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'static_pages#top'
 end
