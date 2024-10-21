9f71b3a1c3152cdc2ef22aa8f91f25461209fd93
TOSHI-54a <skyjoker.toshi.n25@docomo.ne.jp>
2024-10-21 18:32:25 +0000

    Fix end_duration migration with data migration approach

--- db/migrate/20241021181635_change_end_duration_type_in_boards2.rb
+++ db/migrate/20241021181635_change_end_duration_type_in_boards2.rb
@@ -1,13 +1,36 @@
 class ChangeEndDurationTypeInBoards2 < ActiveRecord::Migration[7.0]
   def up
     if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-      change_column :boards, :end_duration, 'timestamp USING end_duration::timestamp without time zone'
+      add_column :boards, :end_duration_temp, :timestamp
+
+      Board.reset_column_information
+
+      Board.find_each do |board|
+        if board.end_duration.present?
+          board.update_column(:end_duration_temp, board.end_duration.to_s)
+        end
+      end
+
+      remove_column :boards, :end_duration
+
+      rename_column :boards, :end_duration_temp, :end_duration
     end
   end
 
   def down
     if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-      change_column :boards, :end_duration, :date
+      add_column :boards, :end_duration_temp, :date
+
+      Board.reset_column_information
+
+      Board.find_each do |board|
+        if board.end_duration.present?
+          board.update_column(:end_duration_temp, board.end_duration.to_date)
+        end
+      end
+
+      remove_column :boards, :end_duration
+      rename_column :boards, :end_duration_temp, :end_duration
     end
   end
 end
