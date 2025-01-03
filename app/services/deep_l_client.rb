ede87e9c0988655adb85afd1299853d5d4590ccf
apple <apple@Tottotto-MacBook-Air.local>
2025-01-04 02:23:14 +0900

    Update:TravelSearchView

--- app/services/deep_l_client.rb
+++ app/services/deep_l_client.rb
@@ -17,7 +17,8 @@ class DeepLClient
     response = self.class.post('https://api-free.deepl.com/v2/translate', headers: @headers, body: {
                                  auth_key: @api_key,
                                  text: text,
-                                 target_lang: 'JA'
+                                 target_lang: 'JA',
+                                 preserve_formatting: 1
                                })
     parsed_response(response)
   end
